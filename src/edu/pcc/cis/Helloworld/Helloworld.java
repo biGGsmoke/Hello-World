f2fb346f4d5668d433ac9da42488d39a51f0eb20
Hezekiah Bernadas <hezekiah.bernadas@pcc.edu>
2023-11-01 02:21:52 -0700

    Added my name

--- src/edu/pcc/cis/Helloworld/Helloworld.java
+++ src/edu/pcc/cis/Helloworld/Helloworld.java
@@ -3,7 +3,7 @@ package edu.pcc.cis.Helloworld;
 import java.util.Date;
 
 /**
- * Created by: Berna on 11/1/2023.
+ * Author: Hezekiah Bernadas
  */
 public class Helloworld {
     public static void main(String[] args) {
